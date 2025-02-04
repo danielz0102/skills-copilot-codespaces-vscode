0907996636713d75cb82b1ce865c551847e786c7
Daniel Zambrano <danyzglez@hotmail.com>
2025-02-04 19:50:09 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    return {
+        name: 'skills',
+        label: 'Skills',
+        icon: 'fa fa-star',
+        color: '#00B0FF',
+        template: 'skillsMember.html',
+        permission: 'member.skills'
+    };
+}
